c63e841d26bf3b6fad3fb9c3f78f1009a2fba4bb
rehaanaa <rehensina@gmail.com>
2025-03-13 21:24:15 +0530

    app.py

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, Render!'
+import os
+
+port = int(os.environ.get('PORT', 10000))  # Default port is 10000
+app.run(host='0.0.0.0', port=port)
